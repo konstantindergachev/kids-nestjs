dc926cfd5558e4c8f70510a68937c2927c2402af
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2021-10-28 12:23:39 +0300

    Used the auth guard for the tales routes

--- src/tale/tale.controller.ts
+++ src/tale/tale.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { AuthGuard } from '@app/user/guards/auth.guard';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ITaleResponse } from './interfaces/tale.interface';
 import { ITalesResponse } from './interfaces/tales.interface';
 import { TaleService } from './tale.service';
@@ -6,15 +7,19 @@ import { TaleService } from './tale.service';
 @Controller('tales')
 export class TaleController {
   constructor(private readonly taleService: TaleService) {}
+
   @Get('first-pages')
+  @UseGuards(AuthGuard)
   async findAllFirstPages(): Promise<ITalesResponse> {
     return await this.taleService.findAllFirstPages();
   }
   @Get()
+  @UseGuards(AuthGuard)
   async findAll(): Promise<ITalesResponse> {
     return await this.taleService.findAll();
   }
   @Get(':slug')
+  @UseGuards(AuthGuard)
   async findOneBySlug(@Param('slug') slug: string): Promise<ITaleResponse> {
     const tale = await this.taleService.findOneBySlug(slug);
     return this.taleService.buildTaleResponce(tale);
