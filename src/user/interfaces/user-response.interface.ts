b378b39e15770885bb3997f17edc138ee49e8429
Kostyantyn Dergachov <dergachevkonstantin@gmail.com>
2022-01-28 19:25:41 +0200

    Renamed the interface

--- src/user/interfaces/user-response.interface.ts
+++ src/user/interfaces/user-response.interface.ts
@@ -1,6 +1,6 @@
 import { UserType } from '../types/user.type';
 
-export interface IUserRegisterResponse {
+export interface IUserSuccessResponse {
   success: boolean;
   message: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,10 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
-import { IUserRegisterResponse } from './interfaces/user-response.interface';
+import {
+  IUserSuccessResponse,
+  IUserResponse,
+} from './interfaces/user-response.interface';
 import { UserService } from './user.service';
 import { generateJWT } from './utils';
 import { UserEntity } from './user.entity';
@@ -15,21 +18,23 @@ export class UserController {
   @Post('/')
   async createUser(
     @Body() createUserDto: CreateUserDto,
-  ): Promise<IUserRegisterResponse> {
+  ): Promise<IUserSuccessResponse> {
     await this.userService.createUser(createUserDto);
     return { success: true, message: REGISTRATION_SUCCESS };
   }
 
   @Post('/login')
   @HttpCode(200)
-  async loginUser(@Body() loginUserDto: LoginUserDto) {
+  async loginUser(@Body() loginUserDto: LoginUserDto): Promise<IUserResponse> {
     const user = await this.userService.loginUser(loginUserDto);
     const token = this.preUserResponse(user);
     return { user: { ...user, token } };
   }
   @Post('/news')
   @HttpCode(200)
-  async sendNews(@Body() newsUserDto: NewsUserDto) {
+  async sendNews(
+    @Body() newsUserDto: NewsUserDto,
+  ): Promise<IUserSuccessResponse> {
     await this.userService.sendNewsLetter(newsUserDto);
     return { success: true, message: SEND_NEWS_MESSAGE };
   }
